4fd6a1fb29ae3bf500105ffa8f4a5588c9e21552
Julian Bejarano <Julianbejarano314@gmail.com>
2025-09-02 20:07:26 -0500

    Repaso Javascript

---
+++ 01-holaMundo/holamundo.js
@@ -0,0 +1,30 @@
+console.log('hola mundo')
+
+let cajaDeAndy = "Woody";
+
+console.log(cajaDeAndy);
+
+let edad = 17;
+let nombre = "Julian"
+
+console.log("Mi nombre es " + nombre + " y tengo " + edad + " años.");
+
+//Array
+
+let frutas =['manzana','banano','mango'];
+
+console.log(frutas);
+
+console.log(frutas[1]);
+
+let frase = "JavaScript es extremadamente genial";
+
+console.log(frase);
+console.log(frase.length);
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+
+//operaciones matematicas
+const suma = 3 +12;
+
+console.log(suma);
